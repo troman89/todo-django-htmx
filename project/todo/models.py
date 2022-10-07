4ee3d9b1398f2e9dced22ce3a8508d9bf17a4376
troman89 <troman89@yahoo.com>
2022-10-07 19:19:06 +0200

    + model todo

--- project/todo/models.py
+++ project/todo/models.py
@@ -5,3 +5,10 @@ from django.db.models.functions import Lower
 
 class User(AbstractUser):
     pass
+
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+    order = models.PositiveSmallIntegerField(default=1)
--- project/todo/urls.py
+++ project/todo/urls.py
@@ -1,8 +1,17 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', include('todo.urls')),
-    path('admin/', admin.site.urls),
-]
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    ]
 
+
+htmx_urlpatterns = [
+    path('check_username/', views.check_username, name="check_username"),
+    ]
+
+urlpatterns += htmx_urlpatterns
